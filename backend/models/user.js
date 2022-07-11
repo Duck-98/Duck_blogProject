d63baac0d27c445e54aa7ab0d0767e33251a4800
duck <godqhr2256@gmail.com>
2022-07-11 16:13:08 +0900

    error 되돌리기

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    "User", // id가 기본적으로 들어가있기 때문에 만들지 않아도 됨.
+    'User', // id가 기본적으로 들어가있기 때문에 만들지 않아도 됨.
     {
       email: {
         type: DataTypes.STRING(30),
@@ -17,23 +17,23 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
     {
-      charset: "utf8",
-      collate: "utf8_general_ci", // 한글 저장
+      charset: 'utf8',
+      collate: 'utf8_general_ci', // 한글
     },
   );
   User.associate = (db) => {
     db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
-    db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" });
+    db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
     db.User.belongsToMany(db.User, {
-      through: "Follow",
-      as: "Followers",
-      foreignKey: "FollowingId",
+      through: 'Follow',
+      as: 'Followers',
+      foreignKey: 'FollowingId',
     });
     db.User.belongsToMany(db.User, {
-      through: "Follow",
-      as: "Followings",
-      foreignKey: "FollowerId",
+      through: 'Follow',
+      as: 'Followings',
+      foreignKey: 'FollowerId',
     });
   };
   return User;
